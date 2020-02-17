3a6ecb83696836636cac862247c3cfef19002e6c
muniraj1152 <muniraj1152@gmail.com>
2020-02-17 20:38:30 +0530

    Check Vue and Vuex Found   - Import modules into store file

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const fs = require("fs-extra");
+import fs from 'fs';
 const commandLineArgs = require("command-line-args");
 
 import {ComponentGenerator } from "./src/generate/component/ComponentGenerator";
@@ -11,7 +11,22 @@ const optionDefinitions = [
 ];
 const options = commandLineArgs(optionDefinitions);
 
-options.isRootFolder= fs.existsSync(`${process.cwd()}/package.json`) ? true : false;
+options.isRootFolder = fs.existsSync(`${process.cwd()}/package.json`) ? true : false;
+options.typescript = fs.existsSync(`${process.cwd()}/tsconfig.json`) ? true : false;
+if (options.isRootFolder) {
+  let content = fs.readFileSync(`${process.cwd()}/package.json` , "utf8");
+  const json = JSON.parse(content);
+  if(json && json.dependencies) {
+    if(json.dependencies.vue) {
+      options.vue = true;
+    }
+    if(json.dependencies.vuex) {
+      options.vuex = true;
+    }
+  }
+} else {
+  console.log("Vue Project not found");
+}
 
 if (options.component) {
   options.isCompontsFolderExist =fs.existsSync(`${process.cwd()}/src/components`) ? true : false;
@@ -19,4 +34,24 @@ if (options.component) {
 } else if (options.module) {
 } else {
   console.log("command not found");
+}
+
+/**
+ * <p>
+ * Importing created module into store
+ * </p>
+ * @param moduleName - module name
+ * @param storeFile - path for store location
+ * @param type - file type
+ */
+let importToStore = (moduleName:string, storeFile:string, type:string) => {
+  let content = fs.readFileSync(storeFile, "utf8");
+  content = content.replace(/modules: {/i, (substring: string) => {
+    return `${substring}\n  ${moduleName}`;
+  });
+  let index = content.lastIndexOf("import ");
+  content = content.slice(0,index) + content.slice(index).replace(/import .* from .*;/gi, (substring: string) => {
+    return `${substring} \n import { ${moduleName} } from "./${moduleName}/index.${type}";`
+  });
+  fs.writeFileSync(storeFile, content);
 }
